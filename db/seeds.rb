8386f1d6dfca80da11872573a118d5b6a3f4bce6
Fred <cherniavsky8fedor@gmail.com>
2017-05-09 14:19:09 +0300

    fix test

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 Product.create!(title: 'Programming ruby 1.9 & 2.0',
   description:
     %{<p>
-      Ruby is the fastest growing and most exciting dynamic language
-      out there. If u need to get working programs delivered fast,
-      yo should add Ruby to your toolbox.
+    Ruby is the fastest growing and most exciting dynamic
+    language out there. If you need to get working programs
+    delivered fast, you should add Ruby to your toolbox.
     </p},
     image_url: 'ruby.jpg',
     price: 49.95)
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -30,29 +30,42 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   def new_product(image_url)
-    Product.new(title:     "My book", description:         "yyy", price: 1, image_url: image_url)
+    Product.new(title: "My cool book", description: "yyy", price: 1, image_url: image_url)
   end
 
   test "image url" do
     ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg https://a.b.c/x/y/z/fred.gif }
-    bad %w{fred.doc fred.gif/more fred.gif.more}
-    ok each do |name|
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+    ok.each do |name|
+      #puts [name, new_product(name), new_product(name).valid?].inspect --- vozrawayemie danie vivodit v consol`
+      #a = new_product(name)
+      #a.valid?
+      #puts a.errors.inspect
       assert new_product(name).valid?, "#{name} shouldn't be invalid"
-  end
-
-  bad.each do |name|
-    assert new_product(name).invalid? "#{name} shouldn't be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid? "#{name} shouldn't be valid... will be stope, use more correct valid"
+    end
   end
 
   test "product is not valid without a unique title" do
-    product = Product.new(title: product(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
 
   test "product is not valid without a unique title - i18n" do
-    product = Product.new(title:     product(:ruby).title, description:  "yyy", price:    1, image_url:   "fred.gif")
+    product = Product.new(title: products(:ruby).title, description: "yyy", price: 1, image_url: "fred.gif")
     assert product.invalid?
-    assert_equal [I18n.translate('activerecord.errors.message.take')], product.errors[:title]
+    assert_equal [I18n.t('errors.messages.taken')], product.errors[:title]
+  end
+
+  test "product is not valid without a unique titl" do
+    product = Product.new(title: 'short', description: "yyy", price: 1, image_url: "fred.gif")
+    #product.invalid?
+    #puts product.errors.inspect
+    #puts product.errors[:title].inspect
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 10 characters)"], product.errors[:title]
   end
 end
