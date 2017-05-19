ab051c571bc8fae1db7898e507b09b1d44c16695
Fred <cherniavsky8fedor@gmail.com>
2017-05-19 13:53:44 +0300

    Prevention display empty cart and fix my error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.iu.effect-blind
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require jquery-ui
