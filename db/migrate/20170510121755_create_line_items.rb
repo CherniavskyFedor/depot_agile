2d81a28679d07c5b49197b04a8995f9c1e41a21d
Fred <cherniavsky8fedor@gmail.com>
2017-05-30 16:58:49 +0300

    commit for dp base + info

--- db/migrate/20170510121755_create_line_items.rb
+++ db/migrate/20170510121755_create_line_items.rb
@@ -1,8 +1,8 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
-      t.references :product, index: true
-      t.belongs_to :cart, index: true
+      t.references :product, index: true # referencet eto toje samoe 4to i belongs_to
+      t.belongs_to :cart, index: true #sozdaet celo4islenoe pole s imenem cart_id
       t.timestamps
     end
   end
--- db/migrate/20170512121102_add_quantity_to_line_items.rb
+++ db/migrate/20170512121102_add_quantity_to_line_items.rb
@@ -1,5 +1,10 @@
 class AddQuantityToLineItems < ActiveRecord::Migration
   def change
     add_column :line_items, :quantity, :integer, default: 1
+    #add_column - metod kotorii dobavlyaet ya4eiky v bazy
+    #1argyment - eto imya tablici v kotoryu ya4eika bydet dobavlena
+    #2argyment - imya ya4eiki
+    #3argyment - tip danix etoi ya4eiki
+    #4argyment - dopolnitel`nie parametri
   end
 end
