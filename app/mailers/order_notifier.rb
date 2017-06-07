3f8fb639c989a719d789bae762759e21b9870635
Fred <cherniavsky8fedor@gmail.com>
2017-06-07 04:11:24 +0300

    Sending confirmation e-mails

---
+++ app/mailers/order_notifier.rb
@@ -0,0 +1,25 @@
+class OrderNotifier < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: 'Order confirmation in Pragmatic Store'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_notifier.shipped.subject
+  #
+  def shipped
+    @order = order
+
+    mail to: order.email, subject: 'Order from Pragmatic Store has been sent.'
+  end
+end
